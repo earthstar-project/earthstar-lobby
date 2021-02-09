0823743344f930ddc482d7c6dcce058fc42b30f6
Sam Gwilym <gwilym@me.com>
2021-02-09 08:41:20 +0000

    Sort workspaces by most recently posted to

--- src/WorkspaceViewer.tsx
+++ src/WorkspaceViewer.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import MessageComposer from "./MessageComposer";
 import WorkspaceMessages from "./WorkspaceMessages";
 import { useCurrentAuthor } from "react-earthstar";
-import Button from "./Button";
 import { LobbyContext } from "./util/lobby-context";
-import MaxWidth from "./MaxWidth";
 
 type WorkspaceViewerProps = {
   workspaceAddress: string;
@@ -14,7 +12,6 @@ const WorkspaceViewer: React.FC<WorkspaceViewerProps> = ({
   workspaceAddress,
 }) => {
   const [currentAuthor] = useCurrentAuthor();
-  const { appStateDispatch } = React.useContext(LobbyContext);
 
   return (
     <>
